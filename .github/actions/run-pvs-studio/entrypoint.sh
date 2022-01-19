723f2ee1ef8fcc6792a2e8ac4f1ae4eead937f80
Noam Arye Nassi <noam@asflabs.com>
2022-01-19 18:29:09 +0200

    ahm

--- .github/actions/run-pvs-studio/entrypoint.sh
+++ .github/actions/run-pvs-studio/entrypoint.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
+pvs-studio-analyzer credentials PVS-Studio Free FREE-FREE-FREE-FREE
 mkdir -p /pvs_workdir/cmake-build
 pushd /pvs_workdir/cmake-build
 cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=On /github/workspace
-pwd
-ll
 pvs-studio-analyzer analyze -f compile_commands.json -o pvs.log -j8
 plog-converter -a GA:1,2 -t tasklist -o "/github/workspace$1" pvs.log
 popd
