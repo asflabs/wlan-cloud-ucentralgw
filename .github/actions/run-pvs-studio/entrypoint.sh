ee7b0046d46cd6e044d664dfc0a55f6ccb66fd34
Noam Arye Nassi <noam@asflabs.com>
2022-01-19 16:09:19 +0200

    yes.

---
+++ .github/actions/run-pvs-studio/entrypoint.sh
@@ -0,0 +1,6 @@
+mkdir -p /pvs_workdir/cmake-build
+pushd /pvs_workdir/cmake-build
+cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=On /github/workspace
+pvs-studio-analyzer analyze -f compile_commands.json -o pvs.log -j8
+plog-converter -a GA:1,2 -t tasklist -o "/github/workspace$1" pvs.log
+popd
